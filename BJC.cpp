9b410ef836754d02ba9ee6327bada0a4df12b1ee
Jeremy-Marbles <marblekid15@gmail.com>
2025-01-01 10:07:07 -0800

    REFACTOR createDeck

--- BJC.cpp
+++ BJC.cpp
@@ -9,7 +9,7 @@ enum class suits
 };
 
 enum class values {
-	Begin, TWO = Begin, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+	Begin, TWO = Begin, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
 	JACK, QUEEN, KING, ACE, End
 };
 
@@ -21,57 +21,13 @@ struct Card {
 //global variables
 std::vector<Card> deck;
 
-void createDeck(){
-	/* TESTCASE:
-	 * Card generatedCard;
-	generatedCard = {suits::HEARTS, values::TWO};
-	deck.push_back(generatedCard);
-	generatedCard = {suits::HEARTS, values::THREE};
-	deck.push_back(generatedCard);*/
-	Card newCard;
-	//add switch case for loop here
-	for (int i = 0; i < 13; i++){
-		switch (i) {
-		 case 0:
-		  newCard = {suits::HEARTS, values::TWO};
-		  deck.push_back(newCard);
-		  break;
-		 case 1:
-		  newCard = {suits::HEARTS, values::THREE};
-		  deck.push_back(newCard);
-		  break;
-		 case 2:
-		  newCard = {suits::HEARTS, values::FOUR};
-		  deck.push_back(newCard);
-		  break;
-		 case 3:
-		  newCard = {suits::HEARTS, values::FIVE};
-		  deck.push_back(newCard);
-		  break;
-		 case 4:
-		  newCard = {suits::HEARTS, values::SIX};
-		  deck.push_back(newCard);
-		  break;
-		 case 5:
-		  newCard = {suits::HEARTS, values::SEVEN};
-		  deck.push_back(newCard);
-		  break;
-		 case 6:
-		  newCard = {suits::HEARTS, values::SEVEN};
-		  deck.push_back(newCard);
-		  break;
-		 case 7:
-		  newCard = {suits::HEARTS, values::EIGHT};
-		  deck.push_back(newCard);
-		  break;
-		 case 8:
-		  newCard = {suits::HEARTS, values::NINE};
-		  deck.push_back(newCard);
-		  break;
-		 case 9:
-		  newCard = {suits::HEARTS, values::TEN};
-		  deck.push_back(newCard);
-		  break;
+void createDeck() {
+	for (int i = 0; i < 4; i++) 
+	{
+		for (int j = 0; j < 13; j++) 
+		{
+			Card newCard = {static_cast<suits>(i), static_cast<values>(j)};
+			deck.push_back(newCard);
 		}
 	}
 }
