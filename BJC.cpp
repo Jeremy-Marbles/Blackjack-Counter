b9be904723051c77a2b6d03d5768699fb73b7c22
Jeremy-Marbles <marblekid15@gmail.com>
2025-01-01 21:17:44 -0800

    Init: shuffler in vector

--- BJC.cpp
+++ BJC.cpp
@@ -1,5 +1,7 @@
 //blackjack_c(ard)_counter.cpp
 #include <iostream>
+#include <string>
+#include <algorithm>
 #include <vector>
 #include <random>
 
@@ -19,7 +21,10 @@ struct Card {
 };
 
 //global variables
-std::vector<Card> deck;
+//to be added: Multiple decks as per rules in casinos
+std::vector<Card> deck; //initial deck
+std::vector<Card> deck_2; 
+
 
 void createDeck() {
 	for (int i = 0; i < 4; i++) 
@@ -32,6 +37,18 @@ void createDeck() {
 	}
 }
 
+void shuffleDeck() 
+{
+	//seed random number generator for shuffling
+	auto RNG = std::mt19937{std::random_device()()};
+	std::uniform_int_distribution d(0, 51);
+	for (int i = 0; i < sizeof(deck); i++) 
+	{
+		//get random seed
+
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	std::cout << "Blackjack CC" << std::endl;
@@ -39,9 +56,12 @@ int main(int argc, char const *argv[])
 	//deck.push_back(testCard);
 	
 	createDeck();
+	/* TESTCCASE: print out the deck
 	for (const auto& card : deck ) {
 		std::cout << static_cast<int>(card.suit) << " " << static_cast<int>(card.value) << std::endl;
 	}
+	*/
+
 	
 	return 0;
 }
